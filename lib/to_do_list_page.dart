90c163678d4000cb5b5420a4cf294347efd7ad79
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-07-12 09:43:43 -0300

    adicionando informações do usuário

--- lib/to_do_list_page.dart
+++ lib/to_do_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 
 class ToDoListPage extends StatefulWidget {
   const ToDoListPage({Key? key}) : super(key: key);
@@ -14,12 +16,33 @@ class ToDoListPage extends StatefulWidget {
         appBar: AppBar(
           title: const Text('To Do List'),
           backgroundColor: Theme.of(context).primaryColor,
-          decoration: const BoxDecoration(
+          centerTitle: true,
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
           ),
         ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: const [
+                UserAccountsDrawerHeader(
+                    accountName: Text("Adaias Martins"),
+                    accountEmail: Text("adaiasteste@gmail.com"),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Text("AM"),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                )
+              ],
+            ),
+          ),
         body: Container(
           padding: const EdgeInsets.all(24),
-          child: Column(
+          child: const Column(
             children: [
               TextField(),
             ]
