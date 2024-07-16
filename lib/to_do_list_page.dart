f0a748cc29302cfd525e71a28b9e6ec828de92c0
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-07-16 20:46:17 -0300

    adicionando e removendo tarefas

--- lib/to_do_list_page.dart
+++ lib/to_do_list_page.dart
@@ -1,53 +1,100 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/widgets.dart';
 
 class ToDoListPage extends StatefulWidget {
-  const ToDoListPage({Key? key}) : super(key: key);
+  const ToDoListPage({super.key});
 
   @override
   _ToDoListPageState createState() => _ToDoListPageState();
 }
 
-  class _ToDoListPageState extends State<ToDoListPage> {
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('To Do List'),
-          backgroundColor: Theme.of(context).primaryColor,
-          centerTitle: true,
-          titleTextStyle: const TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-          ),
+class _ToDoListPageState extends State<ToDoListPage> {
+  TextEditingController _textEditingController = TextEditingController();
+
+  List<String> listaDeTarefas = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('To Do List'),
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 24,
         ),
-          drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: const [
-                UserAccountsDrawerHeader(
-                    accountName: Text("Adaias Martins"),
-                    accountEmail: Text("adaiasteste@gmail.com"),
-                    currentAccountPicture: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      child: Text("AM"),
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                    ),
-                )
-              ],
-            ),
-          ),
-        body: Container(
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text("Adaias Martins"),
+              accountEmail: Text("adaiasteste@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("AM"),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+      ),
+      body: Container(
           padding: const EdgeInsets.all(24),
-          child: const Column(
-            children: [
-              TextField(),
-            ]
+          child: Column(children: [
+            TextField(
+              controller: _textEditingController,
+            ),
+            Container(
+              height: 300,
+              child: ListView.separated(itemBuilder: (context, index) => Divider(),
+                  separatorBuilder: (context, index){
+                    return ListTile(
+                      title: Text(listaDeTarefas[index]),
+                      onLongPress: () {
+                        setState(() {
+                          listaDeTarefas.removeAt(index);
+                        });
+                      },
+                    );
+                  },
+                  itemCount: listaDeTarefas.length),
+            )
+          ])),
+      floatingActionButton: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              setState(() {
+                listaDeTarefas.add(_textEditingController.text);
+                _textEditingController.clear();
+              });
+            },
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.redAccent,
+            child: const Icon(
+              Icons.remove,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              setState(() {
+                listaDeTarefas.clear();
+              });
+            },
           )
-        )
-        );
-    }
+        ],
+      ),
+    );
   }
+}
